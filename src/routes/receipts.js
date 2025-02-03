b37b6b71727c5cec217d2ae6ddfb720195758e23
mohitgcsiweb <mohit.zaveri@gcsiweb.com>
2025-02-03 15:59:43 -0500

    Tested and made some changes

--- src/routes/receipts.js
+++ src/routes/receipts.js
@@ -12,14 +12,14 @@ router.post('/process', (req, res) => {
         saveReceipt(id, { receipt, points });
         res.json({ id });
     } catch (error) {
-        res.status(400).json({ error: "The receipt is invalid." });
+        res.status(400).json({ error: "The receipt is invalid" });
     }
 });
 
 router.get('/:id/points', (req, res) => {
     const data = getReceipt(req.params.id);
     if (!data) {
-        return res.status(404).json({ error: "No receipt found for that id." });
+        return res.status(404).json({ error: "No receipt found for that id" });
     }
     res.json({ points: data.points });
 });
--- src/services/pointsCalculator.js
+++ src/services/pointsCalculator.js
@@ -1,36 +1,42 @@
 function calculatePoints(receipt) {
     let points = 0;
     
-    // Rule 1: Points for Name - 1 Point each for Character
-    points = points + (receipt.retailer.match(/[a-zA-Z0-9]/g) || []).length;
+    // Rule 1
+    const namePoints = (receipt.retailer.match(/[a-zA-Z0-9]/g) || []).length;
+    points = points + namePoints;
 
-    // Rule 2: Round dollar amount - 50 points
+    // Rule 2:
     if (receipt.total.endsWith('.00')) {
         points = points + 50;
     }
 
-    // Rule 3: Multiple of 0.25 - 25points
-    if (parseFloat(receipt.total) % 0.25 === 0) {
+    // Rule 3: 
+    const totalAmount = parseFloat(receipt.total);
+    const remainder = totalAmount % 0.25;
+    if (remainder === 0) {
         points = points + 25;
     }
 
-    // Rule 4: Every two items - 5 ponits
-    points = points + Math.floor(receipt.items.length / 2) * 5;
+    // Rule 4:
+    const itemPairPoints = Math.floor(receipt.items.length / 2) * 5;
+    points = points + itemPairPoints;
 
-    // Rule 5: Description length multiple of 3 
+    // Rule 5:
     receipt.items.forEach(item => {
-        if (item.shortDescription.trim().length % 3 === 0) {
-            points = points + Math.ceil(parseFloat(item.price) * 0.2);
-        }
+    const trimmedLength = item.shortDescription.trim().length;
+    if (trimmedLength % 3 === 0) {
+        const itemPoints = Math.ceil(parseFloat(item.price) * 0.2);
+        points = points + itemPoints;
+    }
     });
 
-    // Rule 6: Odd day - 6 points
-    const purchaseDay = new Date(receipt.purchaseDate).getDate();
-    if (purchaseDay % 2 === 1) {
-        points = points + 6;
+    // Rule 6:
+    const [year, month, day] = receipt.purchaseDate.split('-').map(Number);
+    if (day % 2 === 1) {
+       points = points + 6;
     }
 
-    // Rule 7: Time between 2:00pm and 4:00pm - 10 Points
+    // Rule 7:
     const [hours, minutes] = receipt.purchaseTime.split(':').map(Number);
     if (hours >= 14 && hours < 16) {
         points = points + 10;
